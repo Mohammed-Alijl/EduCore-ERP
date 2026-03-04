e84865864ac196f9aac1dc501285adf2c77fc98b
Mohammed-Alijl <moh.wa.2016@gmail.com>
2026-03-04 23:54:57 +0200

    feat: make the teacher assigment model and migration

---
+++ database/migrations/2026_03_04_214505_create_teacher_assignments_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teacher_assignments', function (Blueprint $table) {
+            $table->id();
+            
+            $table->foreignId('teacher_id')
+                  ->constrained('teachers')
+                  ->cascadeOnUpdate()
+                  ->cascadeOnDelete();
+                  
+            $table->foreignId('subject_id')
+                  ->constrained('subjects')
+                  ->cascadeOnUpdate()
+                  ->cascadeOnDelete();
+                  
+            $table->foreignId('section_id')
+                  ->constrained('sections')
+                  ->cascadeOnUpdate()
+                  ->cascadeOnDelete();
+                  
+            $table->string('academic_year');
+            
+            $table->timestamps();
+
+            $table->unique(
+                ['teacher_id', 'subject_id', 'section_id', 'academic_year'], 
+                'teacher_assignment_unique'
+            );
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teacher_assignments');
+    }
+};
