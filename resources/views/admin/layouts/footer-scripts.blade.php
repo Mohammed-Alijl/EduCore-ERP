d0371fc627e52594b91672030494f3efec599ae5
Mohammed-Alijl <moh.wa.2016@gmail.com>
2026-03-29 21:57:14 +0300

    feat(agent-rules): add best practices rules for agents

--- resources/views/admin/layouts/footer-scripts.blade.php
+++ resources/views/admin/layouts/footer-scripts.blade.php
@@ -1,36 +1,36 @@
 <!-- Back-to-top -->
 <a href="#top" id="back-to-top"><i class="las la-angle-double-up"></i></a>
 <!-- JQuery min js -->
-<script src="{{URL::asset('assets/admin/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{ URL::asset('assets/admin/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap Bundle js -->
-<script src="{{URL::asset('assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{ URL::asset('assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- Ionicons js -->
-<script src="{{URL::asset('assets/admin/plugins/ionicons/ionicons.js')}}"></script>
+<script src="{{ URL::asset('assets/admin/plugins/ionicons/ionicons.js') }}"></script>
 <!-- Moment js -->
-<script src="{{URL::asset('assets/admin/plugins/moment/moment.js')}}"></script>
+<script src="{{ URL::asset('assets/admin/plugins/moment/moment.js') }}"></script>
 
 <!-- Rating js-->
-<script src="{{URL::asset('assets/admin/plugins/rating/jquery.rating-stars.js')}}"></script>
-<script src="{{URL::asset('assets/admin/plugins/rating/jquery.barrating.js')}}"></script>
+<script src="{{ URL::asset('assets/admin/plugins/rating/jquery.rating-stars.js') }}"></script>
+<script src="{{ URL::asset('assets/admin/plugins/rating/jquery.barrating.js') }}"></script>
 
 <!--Internal  Perfect-scrollbar js -->
-<script src="{{URL::asset('assets/admin/plugins/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
-<script src="{{URL::asset('assets/admin/plugins/perfect-scrollbar/p-scroll.js')}}"></script>
+<script src="{{ URL::asset('assets/admin/plugins/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ URL::asset('assets/admin/plugins/perfect-scrollbar/p-scroll.js') }}"></script>
 <!--Internal Sparkline js -->
-<script src="{{URL::asset('assets/admin/plugins/jquery-sparkline/jquery.sparkline.min.js')}}"></script>
+<script src="{{ URL::asset('assets/admin/plugins/jquery-sparkline/jquery.sparkline.min.js') }}"></script>
 <!-- Custom Scroll bar Js-->
-<script src="{{URL::asset('assets/admin/plugins/mscrollbar/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+<script src="{{ URL::asset('assets/admin/plugins/mscrollbar/jquery.mCustomScrollbar.concat.min.js') }}"></script>
 <!-- right-sidebar js -->
-<script src="{{URL::asset('assets/admin/plugins/sidebar/sidebar-rtl.js')}}"></script>
-<script src="{{URL::asset('assets/admin/plugins/sidebar/sidebar-custom.js')}}"></script>
+<script src="{{ URL::asset('assets/admin/plugins/sidebar/sidebar-rtl.js') }}"></script>
+<script src="{{ URL::asset('assets/admin/plugins/sidebar/sidebar-custom.js') }}"></script>
 <!-- Eva-icons js -->
-<script src="{{URL::asset('assets/admin/js/eva-icons.min.js')}}"></script>
+<script src="{{ URL::asset('assets/admin/js/eva-icons.min.js') }}"></script>
 @yield('js')
 <!-- Sticky js -->
-<script src="{{URL::asset('assets/admin/js/sticky.js')}}"></script>
+<script src="{{ URL::asset('assets/admin/js/sticky.js') }}"></script>
 <!-- custom js -->
-<script src="{{URL::asset('assets/admin/js/custom.js')}}"></script><!-- Left-menu js-->
-<script src="{{URL::asset('assets/admin/plugins/side-menu/sidemenu.js')}}"></script>
+<script src="{{ URL::asset('assets/admin/js/custom.js') }}"></script><!-- Left-menu js-->
+<script src="{{ URL::asset('assets/admin/plugins/side-menu/sidemenu.js') }}"></script>
 
 <!-- Theme Toggle Logic -->
 <script>
@@ -53,7 +53,7 @@
         }
 
         themeToggleBtn.on('click', function(e) {
-            e.preventDefault();            
+            e.preventDefault();
             if (body.hasClass('dark-theme')) {
                 body.removeClass('dark-theme');
                 document.documentElement.classList.remove('dark-theme');
