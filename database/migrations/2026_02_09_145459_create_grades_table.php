055d70f6d4b46f4d82aaee9a1aac99af6685f85c
Mohammed-Alijl <moh.wa.2016@gmail.com>
2026-04-02 19:43:34 +0300

    feat(Grade): add image column for the grade

--- database/migrations/2026_02_09_145459_create_grades_table.php
+++ database/migrations/2026_02_09_145459_create_grades_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->text('notes')->nullable();
             $table->boolean('status')->default(1);
             $table->integer('sort_order')->default(0);
+            $table->string('image')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
